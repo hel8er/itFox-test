aa4c9abcb06149425452129963201cb23ac2c2fc
hel8er <helber-alx@mail.ru>
2021-07-18 23:04:31 +0300

    before test deploy

--- news/models.py
+++ news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class News(models.Model):
-    """News model"""
+    """Модель Новости"""
     title = models.CharField(max_length=100)
     text = models.TextField()
     pub_date = models.DateField(auto_now=True)
